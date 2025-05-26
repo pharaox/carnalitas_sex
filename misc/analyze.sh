e8f37e919907cd3a697835d5800b5a1dc621ed13
pharaox <pharaox@tutanota.com>
2025-05-26 11:45:43 +0300

    Avoid drama from sex with consorts, slaves, and raped prisoners

--- misc/analyze.sh
+++ misc/analyze.sh
@@ -4,4 +4,5 @@ LOGFILE=debug.log
 
 # Rape events
 echo "Rape interaction accepted: $(grep -a "Rape interaction accepted" debug.log | wc -l)"
+echo "Raped prisoner got pregnant: $(grep -a "Raped prisoner got pregnant" debug.log | wc -l)"
 echo
